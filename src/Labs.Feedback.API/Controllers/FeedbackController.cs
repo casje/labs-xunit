fec6750a01d4c60e86f314bc7d49d23f9c8909a2
casje <claudiojunior.net@gmail.com>
2021-03-06 18:13:25 -0300

    Solucao inicial

---
+++ src/Labs.Feedback.API/Controllers/FeedbackController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Labs.Feedback.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+    }
+}
